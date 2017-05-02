17bff7d7b49a1a8c1e78200612aa85baeff014f8
Colleen Murphy <colleen.murphy@suse.com>
2017-05-02 16:26:16 +0200

    Fix beaker on xenial
    Since the beaker jobs are being run on xenial, we need a special nodeset
    for it, otherwise beaker gives an error:
    
     beaker-hostgenerator was not able to use this value as input.
     Exiting with an Error.
    
    We also want to install puppet from the Ubuntu repos rather than from
    puppetlabs, since puppetlabs doesn't support puppet 3 for Xenial. For
    centos we can keep the install process the same.
    
    Also run django.setup() since django >=1.7 now requires it[1].
    
    Finally, correct the cron check string in the spec tests.
    
    [1] http://django.readthedocs.io/en/latest/releases/1.7.html#backwards-incompatible-changes-in-1-7
    
    Change-Id: Ifd2244ae9dd212b2475f9cd6adb994bc058a4769
    Depends-On: I02729bc2d49f10a37e9314632b229fbbe72d0b0b

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -155,6 +155,6 @@ describe 'puppet-graphite module', :if => ['debian', 'ubuntu'].include?(os[:fami
   end
 
   describe cron do
-    it { should have_entry('0 2 * * * find /var/lib/graphite/storage/whisper -type f -mtime +270 -name \\\\*.wsp -delete; find /var/lib/graphite/storage/whisper -depth -type d -empty -delete > /dev/null').with_user('root') }
+    it { should have_entry('0 2 * * * find /var/lib/graphite/storage/whisper -type f -mtime +270 -name \*.wsp -delete; find /var/lib/graphite/storage/whisper -depth -type d -empty -delete > /dev/null').with_user('root') }
   end
 end
